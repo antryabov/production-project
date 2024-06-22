859d8ee2b74aa4992bdec04fe742b76aa0e8aa85
Anton Ryabov <antonryabovtmb@yandex.ru>
2024-06-22 17:13:47 +0300

    refactor: change address in gitignore

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,20 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { BuildOptions, BuildPaths } from './types/config';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+export function buildPluling({
+	paths
+}: BuildOptions): webpack.WebpackPluginInstance[] {
+	return [
+		new HtmlWebpackPlugin({
+			template: paths.html
+		}),
+		new webpack.ProgressPlugin(),
+		new MiniCssExtractPlugin({
+			filename: 'css/[name].[contentcash:8].css',
+			chunkFilename: 'css/[name].[contentcash:8].css'
+		})
+	];
+}
